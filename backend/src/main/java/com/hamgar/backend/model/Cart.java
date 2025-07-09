8dea92d9a096a59c3b36c9d23193f6a91bdad65e
Hamer Garcia <hamgar28@gmail.com>
2025-07-09 15:01:37 -0500

    final changes

--- backend/src/main/java/com/hamgar/backend/model/Cart.java
+++ backend/src/main/java/com/hamgar/backend/model/Cart.java
@@ -1,12 +1,20 @@
 package com.hamgar.backend.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Cart {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
--- backend/src/main/java/com/hamgar/backend/model/CartItem.java
+++ backend/src/main/java/com/hamgar/backend/model/CartItem.java
@@ -1,13 +1,20 @@
 package com.hamgar.backend.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class CartItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @ManyToOne
     @JoinColumn(name = "producto_id")
     private Producto producto;
